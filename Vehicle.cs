5757c2aeb8f2974cef474623a31a58df04d36cec
RebeccaJones133 <rebecca.jones3@dcmail.ca>
2025-11-16 13:42:45 -0500

    Refactor Vehicle class for CarViewer application
    Updated the Vehicle class to enhance functionality and improve validation.

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,7 +1,11 @@
-﻿// Author:  Kyle Chapman
-// Created: October 16, 2025
-// Updated: October 16, 2025
-// Description: Defines a generic vehicle to be inherited by diferent vehicle types for display.
+// Author:  Rebecca Jones
+// Created: November 15 2025
+// Updated: November 16 2025
+// Description:
+// Abstract base class for all vehicles displayed in the CarViewer application.
+// Tracks shared state like make, model, year, price, condition, and a unique ID
+// for each vehicle instance.
+//Sources: Kyle Chapman CarList2025 was used as my insperation on the basics of this code.
 
 using System;
 using System.Collections.Generic;
@@ -9,79 +13,117 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarList2025
+namespace CarViewer
 {
     internal abstract class Vehicle
     {
+        // Static counter for all vehicles, used to assign an ID.
         protected static int count = 0;
 
-        // Declared and initialized Variables
-        protected string make = String.Empty;
-        protected string model = String.Empty;
+        // Core fields shared by all vehicles.
+        protected string make = string.Empty;
+        protected string model = string.Empty;
         protected int year = DateTime.Now.Year;
-        protected decimal price = 0.0M;
+        protected decimal price = 0.0m;
         protected bool isNew = false;
-        protected int id = count;
+        protected int id;
 
         /// <summary>
-        /// A default constructor
+        /// Default constructor. Increments the shared counter and assigns an ID.
         /// </summary>
-        public Vehicle()
+        protected Vehicle()
         {
             count++;
             id = count;
         }
 
         /// <summary>
-        /// An accessor that returns the make of the car
+        /// The vehicle's manufacturer.
         /// </summary>
-        /// <returns></returns>
-        public string Make { get { return make; } set { make = value; } }
-
-        /// <summary>
-        /// An accessor that returns the model of the car
-        /// </summary>
-        /// <returns></returns>
-        public string Model { get { return model; } set { model = value; } }
-
-        /// <summary>
-        /// An accessor that returns the year of the car
-        /// </summary>
-        /// <returns></returns>
-        public int Year { get { return year; } set { year = value; } }
-
-        /// <summary>
-        /// An accessor that returns the colour of the car
-        /// </summary>
-        /// <returns></returns>
-        public decimal Price { get { return price; } set { price = value; } }
-
-        /// <summary>
-        /// An accessor that returns the if the car is new or old
-        /// </summary>
-        /// <returns></returns>
-        public bool IsNew { get { return isNew; } set { isNew = value; } }
-
-        /// <summary>
-        /// An accessor that returns car's unique identifier.
-        /// </summary>
-        /// <returns></returns>
-        public int IdentificationNumber { get { return id; } }
-
-        /// <summary>
-        /// Returns the number of vehicles created.
-        /// </summary>
-        /// <returns></returns>
-        public static int Count { get { return count; } }
-
-        /// <summary>
-        /// Returns a string version of the vehicle.
-        /// </summary>
-        /// <returns>A string version of the vehicle.</returns>
-        public override string ToString()
+        public string Make
         {
-            return Year + " " + Make + " " + Model;
+            get => make;
+            set => make = value ?? string.Empty;
         }
 
+        /// <summary>
+        /// The vehicle's model. Cannot be null or blank.
+        /// </summary>
+        public string Model
+        {
+            get => model;
+            set
+            {
+                // Just in case,don’t allow empty models.
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    // a error message if the model is blank
+                    throw new ArgumentNullException(nameof(value), "Model cannot be blank.");
+                }
+
+                model = value;
+            }
+        }
+
+        /// <summary>
+        /// The model year of the vehicle.
+        /// </summary>
+        public int Year
+        {
+            get => year;
+            set => year = value;
+        }
+
+        /// <summary>
+        /// The price of the vehicle. Must be zero or greater.
+        /// </summary>
+        public decimal Price
+        {
+            get => price;
+            set
+            {
+                // checks if the price in below zero
+                if (value < 0)
+                {
+                    // error message for negative prices
+                    throw new ArgumentOutOfRangeException(nameof(value), "Price cannot be negative.");
+                }
+
+                price = value;
+            }
+        }
+
+        /// <summary>
+        /// True if the vehicle is new, false if pre-owned.
+        /// </summary>
+        public bool IsNew
+        {
+            get => isNew;
+            set => isNew = value;
+        }
+
+        /// <summary>
+        /// Unique identifier for this vehicle.
+        /// </summary>
+        public int IdentificationNumber => id;
+
+        /// <summary>
+        /// Total number of vehicles created.
+        /// </summary>
+        public static int Count => count;
+
+        /// <summary>
+        /// A short label describing the vehicle type ("Car", "Motorcycle", ect).
+        /// </summary>
+        public abstract string Type { get; }
+
+        /// <summary>
+        /// Returns a nicely formatted string representation of the vehicle.
+        /// </summary>
+        public override string ToString()
+        {
+            string condition = isNew ? "New" : "Used";
+            return $"[{IdentificationNumber}] {Year} {Make} {Model} - {Type} - {condition} - {Price:C}";
+        }
     }
 }
